7d6317e5bebf0a2e8b3e4b24cb734733ddc57ff6
Jessica Hosman <jhosman@oreilly.com>
2013-07-01 19:08:05 -0400

    Commenting out all but one line

--- ex1.py
+++ ex1.py
@@ -1,8 +1,8 @@
-print "Hello World!"
-print "Hello Again!"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
+# print "Hello World!"
+# print "Hello Again!"
+# print "I like typing this."
+# print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
 print 'Then she said, "Whoa." And I was like, "I know!"'
