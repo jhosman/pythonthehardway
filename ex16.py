a31e1e3088703a2feb50c857d23a83f450644bcc
Jessica Hosman <jhosman@oreilly.com>
2013-07-01 20:54:20 -0400

    Adding exercise 16 file

---
+++ ex16.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, filename = argv
+
+print "We're going to erase %r." % filename
+print "If you don't want that, hit CTRL-C (^C)."
+print "If you do want that, hit RETURN."
+
+raw_input("?")
+
+print "Opening the file..."
+target = open(filename, 'w')
+
